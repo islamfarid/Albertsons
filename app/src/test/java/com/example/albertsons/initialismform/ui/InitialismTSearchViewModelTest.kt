e8a5f16d49042b1ed74cc1accb9fe72a06a798d4
islamfarid <islam.farid2100@gmail.com>
2022-12-14 21:34:12 -0500

    adding tests

--- app/src/test/java/com/example/albertsons/initialismform/ui/InitialismTSearchViewModelTest.kt
+++ app/src/test/java/com/example/albertsons/initialismform/ui/InitialismTSearchViewModelTest.kt
@@ -6,6 +6,7 @@ import com.example.albertsons.initialismform.models.ui.InitialismUiState
 import com.example.albertsons.initialismform.usecase.GetInitialismUseCase
 import com.example.albertsons.initialismform.usecase.PrepareInitialismResultForUiUseCase
 import com.example.albertsons.testObserver
+import io.mockk.coEvery
 import io.mockk.mockk
 import junit.framework.TestCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -46,90 +47,17 @@ internal class InitialismTSearchViewModelTest {
             TestCase.assertTrue(uiStateObserver.observedValues.contains(InitialismUiState.Loading))
         }
 
-//    @ExperimentalCoroutinesApi
-//    @Test
-//    fun `GIVEN setup needed  WHEN Login Activity created THEN  notify Ui with SetUp action`() =
-//        mainCoroutineRule.runBlockingTest {
-//            val uiStateObserver = viewModel.loginUiState.testObserver()
-//            coEvery { loginStatusUseCase.execute(any()) } returns false
-//            coEvery { countriesUseCase.execute(Unit) } returns emptyList()
-//
-//            viewModel.onViewCreated()
-//
-//            coVerify { loginStatusUseCase.execute(Unit) }
-//            TestCase.assertTrue(uiStateObserver.observedValues.contains(LoginUIState.SetUp))
-//            TestCase.assertFalse(uiStateObserver.observedValues.contains(LoginUIState.Login))
-//        }
-//
-//    @ExperimentalCoroutinesApi
-//    @Test
-//    fun `GIVEN viewModel creation WHEN Login Activity created THEN  notify Ui with both SetUp and Countries`() =
-//        mainCoroutineRule.runBlockingTest {
-//            val uiStateObserver = viewModel.loginUiState.testObserver()
-//            coEvery { loginStatusUseCase.execute(any()) } returns false
-//            coEvery { countriesUseCase.execute(Unit) } returns emptyList()
-//
-//            viewModel.onViewCreated()
-//
-//            coVerify { loginStatusUseCase.execute(Unit) }
-//            TestCase.assertTrue(uiStateObserver.observedValues.contains(LoginUIState.SetUp))
-//            verifyUiStatesContainsCountries(uiStateObserver)
-//        }
-//
-//    private fun verifyUiStatesContainsCountries(uiStateObserver: TestObserver<LoginUIState>) {
-//        var containsCountriesEvent = false
-//        uiStateObserver.observedValues.forEach {
-//            if (it is LoginUIState.CountriesLoaded) {
-//                containsCountriesEvent = true
-//            }
-//        }
-//        TestCase.assertTrue(containsCountriesEvent)
-//    }
-//
-//    @ExperimentalCoroutinesApi
-//    @Test
-//    fun `GIVEN setup is not needed  WHEN Login Activity created THEN  notify Ui with Login action`() =
-//        mainCoroutineRule.runBlockingTest {
-//            val uiStateObserver = viewModel.loginUiState.testObserver()
-//            coEvery { loginStatusUseCase.execute(any()) } returns true
-//            coEvery { countriesUseCase.execute(Unit) } returns emptyList()
-//
-//            viewModel.onViewCreated()
-//
-//            coVerify { loginStatusUseCase.execute(Unit) }
-//            TestCase.assertFalse(uiStateObserver.observedValues.contains(LoginUIState.SetUp))
-//            TestCase.assertTrue(uiStateObserver.observedValues.contains(LoginUIState.Login))
-//        }
-//
-//    @ExperimentalCoroutinesApi
-//    @Test
-//    fun `GIVEN userName and password ready  WHEN login mode, and user clicks login THEN call login usecase`() =
-//        mainCoroutineRule.runBlockingTest {
-//            val userName = "userName"
-//            val password = "password"
-//            coEvery { loginStatusUseCase.execute(any()) } returns true
-//            coEvery { countriesUseCase.execute(Unit) } returns emptyList()
-//            coEvery { loginUseCase.execute(CurrentUser(userName, password)) } returns true
-//            viewModel.onViewCreated()
-//
-//            viewModel.onLogin(userName, password)
-//
-//            coVerify { loginUseCase.execute(CurrentUser(userName, password)) }
-//        }
-//
-//    @ExperimentalCoroutinesApi
-//    @Test
-//    fun `GIVEN userName and password ready  WHEN setup mode, and user clicks setup THEN call setup usecase`() =
-//        mainCoroutineRule.runBlockingTest {
-//            val userName = "userName"
-//            val password = "password"
-//            coEvery { loginStatusUseCase.execute(any()) } returns false
-//            coEvery { countriesUseCase.execute(Unit) } returns emptyList()
-//            coEvery { loginSetUpUseCase.execute(CurrentUser(userName, password)) } returns true
-//            viewModel.onViewCreated()
-//
-//            viewModel.onLogin(userName, password)
-//
-//            coVerify { loginSetUpUseCase.execute(CurrentUser(userName, password)) }
-//        }
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `GIVEN viewModel Called  WHEN Text Changed THEN  load the list`()=
+        mainCoroutineRule.runBlockingTest {
+            val sf = "test"
+            coEvery { getInitialismUseCase.invoke(sf) } returns emptyList()
+            coEvery { prepareInitialismResultForUiUseCase.invoke(emptyList()) } returns emptyList()
+            val uiStateObserver = viewModel.uiState.testObserver()
+            viewModel.onSfChanged(sf)
+            TestCase.assertTrue(uiStateObserver.observedValues.contains(InitialismUiState.InitialismResultLoaded(
+                emptyList()
+            )))
+        }
 }
